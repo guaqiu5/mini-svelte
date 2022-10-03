84a7e378302a43df3b251d718abd18da0afa2dc4
guaqiu <6947640@qq.com>
2022-10-04 00:13:23 +0800

    feat: parse js in template

--- compile/genCode.js
+++ compile/genCode.js
@@ -1,73 +1,79 @@
 
-    function append(node, target) {
-      target.appendChild(node);
-    }
+function append(node, target) {
+  target.appendChild(node);
+}
 
-    let fragment = document.createDocumentFragment();
-  let div1 = document.createElement('div');
+let fragment = document.createDocumentFragment();
+let div1 = document.createElement('div');
 let t1 = document.createTextNode("\n    ");
 
-        append(
-          t1,
-          div1
-        );
+append(
+  t1,
+  div1
+);
 let span1 = document.createElement('span');
 let t2 = document.createTextNode("瓜⚽️");
 
-        append(
-          t2,
-          span1
-        );
+append(
+  t2,
+  span1
+);
 
-        append(
-          span1,
-          div1
-        );
+append(
+  span1,
+  div1
+);
 let ul1 = document.createElement('ul');
+let t3 = document.createTextNode("\n        ");
+
+append(
+  t3,
+  ul1
+);
 let li1 = document.createElement('li');
-let t3 = document.createTextNode("小米");
+let t4 = document.createTextNode("小米");
 
-        append(
-          t3,
-          li1
-        );
+append(
+  t4,
+  li1
+);
 
-        append(
-          li1,
-          ul1
-        );
+append(
+  li1,
+  ul1
+);
 let li2 = document.createElement('li');
-let t4 = document.createTextNode("滴滴");
+let t5 = document.createTextNode("滴滴");
 
-        append(
-          t4,
-          li2
-        );
+append(
+  t5,
+  li2
+);
 
-        append(
-          li2,
-          ul1
-        );
+append(
+  li2,
+  ul1
+);
 let li3 = document.createElement('li');
-let t5 = document.createTextNode("快手");
+let t6 = document.createTextNode("快手");
 
-        append(
-          t5,
-          li3
-        );
+append(
+  t6,
+  li3
+);
 
-        append(
-          li3,
-          ul1
-        );
+append(
+  li3,
+  ul1
+);
 
-        append(
-          ul1,
-          div1
-        );
+append(
+  ul1,
+  div1
+);
 
-        append(
-          div1,
-          fragment
-        );
+append(
+  div1,
+  fragment
+);
 document.body.appendChild(fragment);
